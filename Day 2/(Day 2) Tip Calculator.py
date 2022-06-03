3639334284655360b83f1a6bf1fc40a749517a3f
Noitcani <noitcani123@gmail.com>
2022-06-03 12:50:34 +0800

    Day 2

---
+++ Day 2/(Day 2) Tip Calculator.py
@@ -0,0 +1,21 @@
+# Welcome
+print("Welcome to the tip calculator!")
+
+# Ask for total bill
+total_bill = float(input("What was the total bill?\n"))
+
+#Ask for tip
+tip_valid = False
+while not tip_valid:
+  tip = int(input("How much tip would you like to give? 10, 12, or 15?\n"))
+  if tip in [10,12,15]:
+    tip_valid = True
+
+# Ask for no. of people
+pax = int(input("How many people to split the bill?\n"))
+
+#Calculate share
+share = round((total_bill*(1+(tip/100)))/pax,2)
+
+#Output share
+print(f"Each person should pay: ${share}")
